8ac53cdd0f742d0029b868da262d4ae9eb8bdc0e
Kouetosine <kouetosine@gmail.com>
2023-05-08 13:29:19 +0100

    3 derniers titres à gérer

---
+++ premiers-pas/traitement.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Document</title>
+</head>
+<body>
+    <div> 
+        <h1>Merci pour votre commande</h1>
+        <ul>
+            <li>Nombre de boîtes commandées:</li>
+            <li>Tarif unitaire: 20€</li>
+            <li>total à payer:</li>
+            <li>Remise: euros</li>
+            <li>Nouveau total à payer:</li>
+        </ul>
+    </div>
+    
+    
+</body>
+</html>
